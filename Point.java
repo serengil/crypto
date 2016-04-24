7306125b4162e3a543000abcb62bbc365aa14860
Sefik Ilkin Serengil <serengil@gmail.com>
2016-04-24 23:03:21 +0300

    Added files via upload

---
+++ Point.java
@@ -0,0 +1,23 @@
+package com.serengil.entity;
+
+import java.math.BigDecimal;
+
+public class Point {
+	
+	BigDecimal pointX;
+	BigDecimal pointY;
+	
+	public BigDecimal getPointX() {
+		return pointX;
+	}
+	public void setPointX(BigDecimal pointX) {
+		this.pointX = pointX;
+	}
+	public BigDecimal getPointY() {
+		return pointY;
+	}
+	public void setPointY(BigDecimal pointY) {
+		this.pointY = pointY;
+	}
+	
+}
