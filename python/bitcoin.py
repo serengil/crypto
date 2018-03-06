0c06a01501a061bd8056e85a6c6fba79980249a0
Sefik Ilkin Serengil <serengil@gmail.com>
2018-03-06 13:18:18 +0300

    ecc based functions consumed in ecc class

--- python/bitcoin.py
+++ python/bitcoin.py
@@ -1,10 +1,10 @@
+import ecc
 import codecs
 import hashlib
 
 #------------------------------------
 
 dump = False
-
 applyBitcoinAddress = True
 
 #------------------------------------
@@ -31,93 +31,6 @@ print("modulo: ", mod)
 print("order of group: ", order)
 
 #------------------------------------
-#elliptic curve based functions
-
-def findModularInverse(a, mod):
-			
-	while(a < 0):
-		a = a + mod
-	
-	#a = a % mod
-	
-	x1 = 1; x2 = 0; x3 = mod
-	y1 = 0; y2 = 1; y3 = a
-	
-	q = int(x3 / y3)
-	t1 = x1 - q*y1
-	t2 = x2 - q*y2
-	t3 = x3 - (q*y3)
-	
-	if dump == True:
-		print("q\tx1\tx2\tx3\ty1\ty2\ty3\tt1\tt2\tt3")
-		print("----------------------------------------------------------------------------")
-		print(q,"\t",x1,"\t",x2,"\t",x3,"\t",y1,"\t",y2,"\t",y3,"\t",t1,"\t",t2,"\t",t3)
-	
-	while(y3 != 1):
-		x1 = y1; x2 = y2; x3 = y3
-		
-		y1 = t1; y2 = t2; y3 = t3
-		
-		q = int(x3 / y3)
-		t1 = x1 - q*y1
-		t2 = x2 - q*y2
-		t3 = x3 - (q*y3)
-		
-		if dump == True:
-			print(q,"\t",x1,"\t",x2,"\t",x3,"\t",y1,"\t",y2,"\t",y3,"\t",t1,"\t",t2,"\t",t3)
-			print("----------------------------------------------------------------------------")
-			print("")
-	
-	while(y2 < 0):
-		y2 = y2 + mod
-	
-	return y2
-
-def pointAddition(x1, y1, x2, y2, a, b, mod):
-	
-	if x1 == x2 and y1 == y2:
-		#doubling
-		beta = (3*x1*x1 + a) * (findModularInverse(2*y1, mod))
-	
-	else:
-		#point addition
-		beta = (y2 - y1)*(findModularInverse((x2 - x1), mod))
-	
-	x3 = beta*beta - x1 - x2
-	y3 = beta*(x1 - x3) - y1
-	
-	x3 = x3 % mod
-	y3 = y3 % mod
-	
-	while(x3 < 0):
-		x3 = x3 + mod
-	
-	while(y3 < 0):
-		y3 = y3 + mod
-	
-	return x3, y3
-
-def applyDoubleAndAddMethod(x0, y0, k, a, b, mod):
-	
-	x_temp = x0
-	y_temp = y0
-	
-	kAsBinary = bin(k) #0b1111111001
-	kAsBinary = kAsBinary[2:len(kAsBinary)] #1111111001
-	#print(kAsBinary)
-	
-	for i in range(1, len(kAsBinary)):
-		currentBit = kAsBinary[i: i+1]
-		#always apply doubling
-		x_temp, y_temp = pointAddition(x_temp, y_temp, x_temp, y_temp, a, b, mod)
-		
-		if currentBit == '1':
-			#add base point
-			x_temp, y_temp = pointAddition(x_temp, y_temp, x0, y0, a, b, mod)
-	
-	return x_temp, y_temp
-
-#------------------------------------
 #subsdiary functions
 
 def hexStringToByte(content):
@@ -137,7 +50,7 @@ privateKey = 1125356301205968582595361922210782354909214769903167223838579036935
 #print("private key: ", privateKey)
 print("private key (hex): ", hex(privateKey)[2:]," (keep this secret!)\n")
 
-publicKey = applyDoubleAndAddMethod(x0, y0, privateKey, a, b, mod)
+publicKey = ecc.applyDoubleAndAddMethod(x0, y0, privateKey, a, b, mod)
 #print("public key: ", publicKey)
 
 publicKeyHex = "04"+hex(publicKey[0])[2:]+hex(publicKey[1])[2:]
