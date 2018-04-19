59b4c194688430fc7825433c0cbd981c9a2e913f
Sefik Ilkin Serengil <serengil@gmail.com>
2018-04-19 17:17:19 +0300

    reference crypto commons

--- python/dsa.py
+++ python/dsa.py
@@ -1,3 +1,5 @@
+import cryptocommons as commons
+
 print("key generation")
 
 base = 3
@@ -30,32 +32,6 @@ print("public key: ",y)
 print("public params: p=",p,", q=",q,", g=",g,"")
 
 #-------------------------------
-def modInverse(number, mod):
-	x1 = 1; x2 = 0; x3 = mod
-	y1 = 0; y2 = 1; y3 = number
-	
-	q = int(x3 / y3)
-	
-	t1 = x1 - q * y1
-	t2 = x2 - q * y2
-	t3 = x3 - q * y3
-	
-	while y3 != 1:
-		x1 = y1;x2 = y2;x3= y3
-		y1 = t1;y2 = t2; y3 = t3
-		
-		q = int(x3 / y3)
-		t1 = x1 - q * y1
-		t2 = x2 - q * y2
-		t3 = x3 - q * y3
-		
-	if y2 < 0:
-		while y2 < 0:
-			y2 = y2 + mod
-	
-	return y2
-
-#-------------------------------
 
 print("signing")
 
@@ -64,7 +40,7 @@ k = 10 #random key
 h = 123
 
 r = pow(g, k, p) % q
-s = modInverse(k, q) * (h + x*r) % q
+s = commons.modInverse(k, q) * (h + x*r) % q
 
 print("signature: (r=",r,", s=",s,")")
 
@@ -77,7 +53,7 @@ h = 123
 #Also, he knows public params -> p, q, g
 #Also, he knows h, (r, s) pair
 
-w = modInverse(s, q)
+w = commons.modInverse(s, q)
 u1 = h * w % q
 u2 = r * w % q
 v = ((pow(g, u1, p) * pow(y, u2, p)) % p) % q
